5b1be58adddabac0f5b485efdf52e2b709b46547
agravity268-sys <agravity268@gmail.com>
2026-09-12 17:31:55 +0530

    Separate mobile and desktop views, reset desktop to blank

---
+++ src/DesktopApp.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function DesktopApp() {
+  return (
+    <div className="min-h-screen bg-white">
+      {/* Starting Desktop View from scratch */}
+    </div>
+  );
+}
+
+export default DesktopApp;
